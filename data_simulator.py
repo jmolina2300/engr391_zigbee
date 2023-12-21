9b4efb386dc0aca5562e7f5720e4c9a6b86be512
Jan <jmolina2300@gmail.com>
2023-12-21 11:22:20 -0500

    Update data simulator
    - data simulator takes a sensor identity as a command line argument
    - data simulator creates a new CSV file with a header if one does
      not exist
    - include original power point slides

--- data_simulator.py
+++ data_simulator.py
@@ -6,39 +6,60 @@
 
 import random
 import time
-import globals
+from datetime import datetime
+import sys
+from utils import file_exists
 from math import floor
 
-num_points = 100
-file_name = globals.DATA_FILE_NAME
 
 
-def write_data(f) :
-    for i in range(0, num_points):
-        line = generate_line(i)
-        f.write(line)
+"""
+    generate_line()
+	
+    Generates one line of random data with an ISO8601 timestamp
+"""
+def generate_line():
+	rnd = floor(random.gauss(66, 5))
+	timestamp = datetime.now().isoformat()
+	line =  timestamp + ',' + str(rnd) + '\r\n'
+	return line
 
-def generate_line(timestamp):
-    rnd = floor(random.gauss(66, 5))
-    line = str(timestamp) + ',' + str(rnd) + '\n'
-    return line
 
-def generate_file_data():
-    print('Generating',num_points,'points...')
-    with open(file_name, 'w') as f:
-        write_data(f)
-        
+"""
+    create_new_sensor_file()
+
+    Creates a new file with the CSV header using the sensor identity
+"""
+def create_new_sensor_file(identity, new_file_name):
+	print('Creating new file',new_file_name)
+	with open(new_file_name, 'w') as f:
+		csv_header = identity + ',values\r\n'
+		f.write(csv_header)
+
+
 def main():
-    counter = 0
-    while (1):
-        with open(file_name, 'a+') as f:
-            line = generate_line(counter)
-            f.write(line)
-            print(line,end='')
-        
-        time.sleep(1)
-        counter += 1
-        
+	if len(sys.argv) < 2:
+		print('Usage: ./data_simulator.py SENSOR_IDENTITY')
+		quit()
+	
+	# Get the sensor identity from the command line
+	identity = sys.argv[1]
+	sensor_file_name = identity + '.csv'
+	
+
+	while (1):
+		# If the file doesn't exist, create it
+		if not file_exists(sensor_file_name):
+			create_new_sensor_file(identity, sensor_file_name)
+
+		# Append a new line of random data to the file
+		with open(sensor_file_name, 'a+') as f:
+			line = generate_line()
+			f.write(line)
+			print(line,end='')
+
+		time.sleep(1)
+		
 
 if __name__ == "__main__" :
-    main()
+	main()
